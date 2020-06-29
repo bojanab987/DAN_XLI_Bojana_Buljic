01c212f2715f0d720caaf122fae2e3f782c2eef8
bojanabb <bojana.buljic@gmail.com>
2020-06-29 21:47:53 +0200

    PrintDoWork method changed, removed one label from MainWindow xaml file

--- Printer/Printer/ViewModel/MainWindowViewModel.cs
+++ Printer/Printer/ViewModel/MainWindowViewModel.cs
@@ -27,16 +27,20 @@ namespace Printer.ViewModel
         public MainWindowViewModel(MainWindow mainOpen)
         {
             main = mainOpen;
+            //add method to DoWork event
             backgroundWorker.DoWork += PrintDoWork;
+            
             backgroundWorker.WorkerReportsProgress = true;
             backgroundWorker.WorkerSupportsCancellation = true;
+            //add method to ProgressChanged event
             backgroundWorker.ProgressChanged += PrintProgressChanged;
+            //add method to RunWorkerCompleted event
             backgroundWorker.RunWorkerCompleted += PrintRunWorkerCompleted;
         }
 
         #region Properties
         /// <summary>
-        /// Text printed into file
+        /// Text input that will be printed into file
         /// </summary>
         private string txtFile;
         public string TxtFile
@@ -50,7 +54,7 @@ namespace Printer.ViewModel
         }
 
         /// <summary>
-        /// File copies property
+        /// File copies property defining how many file copies user inputed
         /// </summary>
         private string fileCopy;
         public string FileCopy
@@ -81,102 +85,93 @@ namespace Printer.ViewModel
         }
 
         /// <summary>
-        /// Property for print progress finished percentage
+        /// Property for showing messages about print progress 
         /// </summary>
-        private string printFinishedInfo;
-        public string PrintFinishedInfo
+        private string messageInfo;
+        public string MessageInfo
         {
-            get { return printFinishedInfo; }
+            get { return messageInfo; }
             set
             {
-                printFinishedInfo = value;
-                OnPropertyChanged("PrintProgress");
-            }
-        }
-
-        /// <summary>
-        /// Warning info message property
-        /// </summary>
-        private string warningInfo;
-        public string WarningInfo
-        {
-            get { return warningInfo; }
-            set
-            {
-                warningInfo = value;
-                OnPropertyChanged("WarningInfo");
+                messageInfo = value;
+                OnPropertyChanged("MessageInfo");
             }
         }
         #endregion
 
-
+        #region Methods
+        /// <summary>
+        /// Method for text printing in document
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PrintDoWork(object sender, DoWorkEventArgs e)
         {
-
-            for(int i=1; i<int.Parse(fileCopy)+1;i++)
+            //parsing input for number of file copies
+            int number = int.Parse(FileCopy);
+            int percentResult = 100 / number;
+            //files creation and writing text in it
+            for (int i = 1; i <= number; i++)
             {
+                //printing lasts for 1000ms
                 Thread.Sleep(1000);
 
                 //if cancellation is requested, progress percentage resets to 0
-                if(backgroundWorker.CancellationPending)
+                if (backgroundWorker.CancellationPending)
                 {
                     e.Cancel = true;
                     backgroundWorker.ReportProgress(0);
                     return;
                 }
                 //naming file copies
-                file = i + "." + DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_" + DateTime.Now.Hour + "_" +
-                    DateTime.Now.Minute;
+                file = string.Format(@"../../{0}.{1}_{2}_{3}_{4}_{5}.txt", i, DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year,
+                                    DateTime.Now.Hour, DateTime.Now.Minute);
 
                 //writing text into file
                 using (StreamWriter sw = new StreamWriter(file))
                 {
                     sw.WriteLine(txtFile);
                 }
-                //
-                if(i==int.Parse(fileCopy))
-                {
-                    //progress bar is 100% if all copies are printed
-                    backgroundWorker.ReportProgress(100);
-                }
-                else
-                {
-                    //progress bar state depending of no of printed file copies
-                    backgroundWorker.ReportProgress(100 / int.Parse(fileCopy) * i);
-                }
+                //invoke method and raise ProgressChanged event
+                backgroundWorker.ReportProgress(percentResult * i);                
             }
-            WarningInfo = " ";
         }
 
+        /// <summary>
+        /// Update label on user interface with message about print progress
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PrintProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             CurrentProgress = e.ProgressPercentage;
-
-            //if progress is finished, print message that printing is finished
-            if(CurrentProgress==100)
-            {
-                PrintFinishedInfo = "Printing is finished.";
-            }
-            else
-            {
-                //print percentage of finished job
-                PrintFinishedInfo = currentProgress.ToString() + "%";
-            }
+            MessageInfo = e.ProgressPercentage.ToString() + "%";
+            
         }
 
+        /// <summary>
+        /// Method for showing state of file printing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PrintRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             if(e.Cancelled)
             {
-                PrintFinishedInfo = "Printing is cancelled.";
-                WarningInfo = " ";
-                _isRunning = false;
+                MessageInfo = "Printing is cancelled.";                
             }
+            //if error occured during printing
             else if(e.Error !=null)
             {
-                printFinishedInfo = e.Error.Message;
+                MessageInfo = e.Error.Message.ToString();
+            }
+            else
+            {
+                //if progress is finished, print message that printing is finished
+                MessageInfo = "Printing is finished.";
             }
         }
+        #endregion
 
         #region Commands
         /// <summary>
@@ -209,7 +204,7 @@ namespace Printer.ViewModel
             else
             {
                 //if its busy it will show message that printing is in progress
-                WarningInfo = "Printing in progress... Please wait.";
+                MessageInfo = "Printing in progress... Please wait.";
             }
         }
 
